3389dc8418ad6ce0b9916c45ebe912707840ffde
Sergey <32011415+leguha@users.noreply.github.com>
2019-07-15 14:51:55 +0300

    Добавлен рут для апи заявок.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('pointOnMap', 'Api\PointOnMapApiController');
+Route::resource('bid', 'Api\BidApiController');
