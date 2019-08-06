944aef3ddf8240f131d12f9bbe214d0cf94932cb
Bronyrus <bronyruschannel@gmail.com>
2019-08-06 14:59:49 +0300

    del

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -25,17 +25,3 @@ function init() {
         myMap.geoObjects.add(placeMark);
     });
 }
-
-$(function(){
-    $(".js-destroy-all").on("click", function() {
-
-        if($(".js-destroy-all").prop("checked")){
-            $(".js-destroy").prop("checked", "checked");
-            console.log('check');
-        }
-        else{
-            $(".js-destroy").prop("checked", "");
-        }
-
-    });
-});
--- resources/views/layouts/profileApp.blade.php
+++ resources/views/layouts/profileApp.blade.php
@@ -138,6 +138,21 @@
             });
 
         });
+
+        
+        $(function(){
+            $(".js-destroy-all").on("click", function() {
+
+                if($(".js-destroy-all").prop("checked")){
+                    $(".js-destroy").prop("checked", "checked");
+                    console.log('check');
+                }
+                else{
+                    $(".js-destroy").prop("checked", "");
+                }
+
+            });
+        });
     });
 
 
