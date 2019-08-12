5f42815d37a861c416734128bc3653c22621aa66
Bronyrus <bronyruschannel@gmail.com>
2019-08-12 15:14:33 +0300

    repr

--- resources/views/admin/representativeList.blade.php
+++ resources/views/admin/representativeList.blade.php
@@ -35,3 +35,37 @@
         </table>
     </div>
 @endsection
+<script>
+$(document).ready(function() {
+    $(document).on('click', '.js-destroy-button-representative', function() {
+        let ids = [];
+
+        $(".js-destroy:checked").each(function(){
+            ids.push($(this).data('placeId'));
+        });
+        
+        console.log(ids);
+
+        let uSure = confirm('Вы действительно хотите удалить?');
+        if(uSure)
+        {
+            $.ajax({
+            headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            dataType: "json",
+            data    : { ids: ids },
+            url     : 'representative/delete',
+            method    : 'delete',
+            success: function (response) {
+                console.log(response);
+                $(".js-destroy:checked").closest('tr').remove();
+                $(".js-destroy").prop("checked", "");
+            },
+            error: function (xhr, err) { 
+                console.log("Error: " + xhr + " " + err);
+            }
+        });
+        // console.log('sss');
+        }
+    });
+});
+</script>
