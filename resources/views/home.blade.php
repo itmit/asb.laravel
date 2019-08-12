885b03742b6c538adbd7158bba63e0db40a4ad01
Bronyrus <bronyruschannel@gmail.com>
2019-08-12 14:50:29 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Главная</h1>
 
-    <div><span>{{ $cities }}</span></div>
+    <div><span>{{ $cities }} город(а)</span></div>
     <div><span>3 представителя</span></div>
     <div><span>8 диспетчеров</span></div>
 @endsection
