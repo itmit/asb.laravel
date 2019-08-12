abf529a1bae52921cfad2da50ca2666f45b9037b
Bronyrus <bronyruschannel@gmail.com>
2019-08-12 17:48:05 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <h1>Клиент {{ $client->name }}</h1>
     <div class="col-sm-12">
+        <a href="">Назад</a>
+    </div>
+    <div class="col-sm-12">
         <div>
             <img src="{{URL::asset($client->user_picture)}}" alt="profile Pic" height="200" width="200">
-            {{ $client->user_picture }}
         </div>
         <div>
             PASSWORD?????
