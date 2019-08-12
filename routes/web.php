d6a91892fce8c1aa265e149b79cef88c358dd960
Bronyrus <bronyruschannel@gmail.com>
2019-08-12 16:37:47 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,6 @@ Route::group(['as' => 'auth.', 'middleware' => 'auth'], function () {
     Route::delete('dispatcher/delete', 'Web\DispatcherWebController@destroy');
     Route::delete('guard/delete', 'Web\GuardWebController@destroy');
     Route::delete('representative/delete', 'Web\RepresentativeWebController@destroy');
-
-    Route::get('client/{id}', 'Web\ClientWebController@clientDetail');
 });
 
 Auth::routes();
