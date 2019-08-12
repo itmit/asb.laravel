df50d699e4127714de88fa8961fbac0907a403f0
Bronyrus <bronyruschannel@gmail.com>
2019-08-12 17:35:18 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.profileApp')
 
 @section('content')
-    <h1>Клиент {{ $client->id }}</h1>
+    <h1>Клиент {{ $client->name }}</h1>
     <div class="col-sm-12">
+        <div>
+            {{ HTML::image('{{ $client->user_picture }}', 'alt text', array('class' => 'css-class')) }}
+        </div>
+        <div>
+            PASSWORD?????
+        </div>
+        <div>
+            {{ $client->note }}
+        </div>
+        <div>
+            {{ $client->is_active }}
+        </div>
     </div>
 @endsection
