c96e6179d3ed714fd6793ebc8bb3b5f9f39527ff
Bronyrus <bronyruschannel@gmail.com>
2019-08-13 11:48:35 +0300

    Update BidWebController.php

--- app/Http/Controllers/Web/BidWebController.php
+++ app/Http/Controllers/Web/BidWebController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Bid;
+use Illuminate\Support\Facades\Auth;
 
 class BidWebController extends BaseWebController
 {
