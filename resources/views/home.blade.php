3e888f2ede84f6dd972086dfad061388c684b15c
Bronyrus <bronyruschannel@gmail.com>
2019-08-14 12:04:03 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,15 +17,15 @@
             <tbody>
                 <tr>
                     <td>Город</td>
-                    <td class="number">2</td>
+                    <td class="number">{{ $cities }}</td>
                 </tr>
                 <tr>
                     <td>Представитель</td>
-                    <td class="number">2</td>
+                    <td class="number">{{ $reprs }}</td>
                 </tr>
                 <tr>
                     <td>Диспетчер</td>
-                    <td class="number">3</td>
+                    <td class="number">{{ $dispathers }}</td>
                 </tr>
             </tbody>
         </table>
