54007c8ccdb3aba38401c9e7f79775afa2465e42
Bronyrus <bronyruschannel@gmail.com>
2019-08-14 11:03:38 +0300

    Update BidWebController.php

--- app/Http/Controllers/Web/BidWebController.php
+++ app/Http/Controllers/Web/BidWebController.php
@@ -55,7 +55,7 @@ class BidWebController extends BaseWebController
                         'status' => $bid->status,
                         'type' => $bid->type,
                         'updated_at' => $bid->updated_at->timezone('Europe/Moscow'),
-                        'created_at' => $bid->created_at->timezone('Europe/Moscow'),
+                        'created_at' => strstr($bid->created_at->timezone('Europe/Moscow'), '.', true),
                         'location' => [
                             'latitude' => $bid->location()->latitude,
                             'longitude' => $bid->location()->longitude
