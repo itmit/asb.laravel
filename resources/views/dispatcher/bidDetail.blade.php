5c09516c8c1a44f209bf49cf17190d7f4391968e
Bronyrus <bronyruschannel@gmail.com>
2019-08-14 15:05:41 +0300

    Update bidDetail.blade.php

--- resources/views/dispatcher/bidDetail.blade.php
+++ resources/views/dispatcher/bidDetail.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.profileApp')
 
 @section('content')
-    <h1>Заявка {{ $bid->id }}</h1>
+    <h1 data-bidID="{{ $bid->id }}">Заявка {{ $bid->id }}</h1>
     <div class="col-sm-12">
         <a href="{{ url()->previous() }}">Назад</a>
     </div>
     <div class="col-sm-12">
         <div>
             Статус: {{ $bid->status }}
-            <select name="" id="">
+            <select id="changeBidStatus">
                 <option value="Accepted">Accepted</option>
                 <option value="PendingAcceptance">PendingAcceptance</option>
                 <option value="Processed">Processed</option>
@@ -61,6 +61,42 @@
     }
 
 
-    
+    $(document).on('change', '#changeBidStatus', function() {
+        let bidNewStatus = $('#changeBidStatus').val();
+        let bidID = data('bidID');
+        console.log(bidID);
+        // $.ajax({
+        //     headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+        //     dataType: "json",
+        //     data    : { bid: bid },
+        //     url     : 'places/getPlacesByBlock',
+        //     method    : 'post',
+        //     success: function (data) {
+        //     let result = '';
+        //     if(data[0].length === 0)
+        //     {
+        //         result += '<tr><td colspan="7">В выбранном разделе ничего нет</td></tr>'
+        //     }
+        //     else
+        //     {
+        //         for(var i = 0; i < data[0].length; i++) {
+        //             result += '<tr>';
+        //             result += '<td><input type="checkbox" data-place-id="' + data[0][i]['id'] + '" name="destoy-place-' + data[0][i]['id'] + '" class="js-destroy"/></td>';
+        //             result += '<td>' + data[0][i]['block'] + '</td>';
+        //             result += '<td>' + data[0][i]['floor'] + '</td>';
+        //             result += '<td>' + data[0][i]['row'] + '</td>';
+        //             result += '<td>' + data[0][i]['place_number'] + '</td>';
+        //             result += '<td>' + data[0][i]['status'] + '</td>';
+        //             result += '<td>' + data[0][i]['price'] + '</td>';
+        //             result += '</tr>';
+        //         }
+        //     }   
+        //     $('tbody').html(result);
+        //     },
+        //     error: function (xhr, err) { 
+        //         console.log(err + " " + xhr);
+        //     }
+        // });
+    });
     </script>
 @endsection
