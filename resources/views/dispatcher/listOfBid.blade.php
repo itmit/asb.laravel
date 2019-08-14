96f8bf174f857f54ad0b9e044eccfaced87f365a
Bronyrus <bronyruschannel@gmail.com>
2019-08-14 11:53:36 +0300

    Update listOfBid.blade.php

--- resources/views/dispatcher/listOfBid.blade.php
+++ resources/views/dispatcher/listOfBid.blade.php
@@ -37,7 +37,7 @@
                             {{ $bid->location()->latitude }} | {{ $bid->location()->longitude }}
                         </td>
                         <td>{{ $bid->type }}</td>
-                        <td>{{ substr($bid->created_at->timezone('Europe/Moscow'), 0, -6); }}</td>
+                        <td>{{ substr($bid->created_at->timezone('Europe/Moscow'), 0, -6) }}</td>
                         <td>{{ $bid->updated_at->timezone('Europe/Moscow') }}</td>
                     </tr>
                 @endforeach
