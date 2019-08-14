9ce7db8241d78fe76e2636d382e7f5411acb8f2b
Bronyrus <bronyruschannel@gmail.com>
2019-08-14 16:59:25 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -33,7 +33,7 @@
     $(document).ready(function()
     {
         $(document).on('click', '.display-location', function() {
-            let clientID = $('#client').first().data('clientID');
+            let clientID = $('h1').data('clientID');
             console.log(clientID);
             $.ajax({
                 headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
