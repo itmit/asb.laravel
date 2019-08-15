78d13d7ac36d65cd9937981c4a80abd860ba151b
Bronyrus <bronyruschannel@gmail.com>
2019-08-15 12:16:20 +0300

    Update script.js

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,27 +1,27 @@
-// let $doc = $(document);
+let $doc = $(document);
 
-// // Функция ymaps.ready() будет вызвана, когда
-// // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-// ymaps.ready(init);
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
 
-// function init() {
-//     let $locations = $('.js-location');
-//     // Создание карты.
-//     myMap = new ymaps.Map("map", {
-//         // Координаты центра карты.
-//         // Порядок по умолчанию: «широта, долгота».
-//         // Чтобы не определять координаты центра карты вручную,
-//         // воспользуйтесь инструментом Определение координат.
-//         center: [$locations.first().data('longitude'), $locations.first().data('latitude')],
-//         // Уровень масштабирования. Допустимые значения:
-//         // от 0 (весь мир) до 19.
-//         zoom: 12
-//     });
+function init() {
+    let $locations = $('.js-location');
+    // Создание карты.
+    myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [$locations.first().data('longitude'), $locations.first().data('latitude')],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 12
+    });
 
-//     $locations.each(function () {
+    $locations.each(function () {
 
-//         // console.log([$(this).data('latitude'), $(this).data('longitude')]);
-//         let placeMark = new ymaps.Placemark([$(this).data('longitude'), $(this).data('latitude')]);
-//         myMap.geoObjects.add(placeMark);
-//     });
-// }
+        // console.log([$(this).data('latitude'), $(this).data('longitude')]);
+        let placeMark = new ymaps.Placemark([$(this).data('longitude'), $(this).data('latitude')]);
+        myMap.geoObjects.add(placeMark);
+    });
+}
