f872b530ad78af0245817d8555f734ee1681101a
Bronyrus <bronyruschannel@gmail.com>
2019-08-15 11:54:07 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -42,36 +42,26 @@
                 method    : 'post',
                 success: function (response) {
                     $('#location').html(
-                    // let $doc = $(document);
+                    // Функция ymaps.ready() будет вызвана, когда
+                    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+                    ymaps.ready(init);
 
-                    // // Функция ymaps.ready() будет вызвана, когда
-                    // // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-                    // ymaps.ready(init);
-
-                    // function init() {
-                    //     let $locations = $('.js-location');
-                    //     // Создание карты.
-                    //     myMap = new ymaps.Map("map", {
-                    //         // Координаты центра карты.
-                    //         // Порядок по умолчанию: «широта, долгота».
-                    //         // Чтобы не определять координаты центра карты вручную,
-                    //         // воспользуйтесь инструментом Определение координат.
-                    //         center: [$locations.first().data('longitude'), $locations.first().data('latitude')],
-                    //         // Уровень масштабирования. Допустимые значения:
-                    //         // от 0 (весь мир) до 19.
-                    //         zoom: 15
-                    //     });
-
-                    //     $locations.each(function () {
-
-                    //         // console.log([$(this).data('latitude'), $(this).data('longitude')]);
-                    //         let placeMark = new ymaps.Placemark([$(this).data('longitude'), $(this).data('latitude')]);
-                    //         myMap.geoObjects.add(placeMark);
-                    //     });
-                    // }
-
-                    
-
+                    function init() {
+                        // Создание карты.
+                        myMap = new ymaps.Map("map", {
+                            // Координаты центра карты.
+                            // Порядок по умолчанию: «широта, долгота».
+                            // Чтобы не определять координаты центра карты вручную,
+                            // воспользуйтесь инструментом Определение координат.
+                            center: [$locations.first().data('longitude'), $locations.first().data('latitude')],
+                            // Уровень масштабирования. Допустимые значения:
+                            // от 0 (весь мир) до 19.
+                            zoom: 15
+                        });
+                            // console.log([$(this).data('latitude'), $(this).data('longitude')]);
+                            let placeMark = new ymaps.Placemark([response['latitude'], response['longitude']]);
+                            myMap.geoObjects.add(placeMark);
+                    }
                     );
                 },
                 error: function (xhr, err) { 
