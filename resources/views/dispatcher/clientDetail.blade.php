cfa6fdf00dcdf0f1f108683a4ee39b0bddd3572c
Bronyrus <bronyruschannel@gmail.com>
2019-08-15 12:06:36 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -30,6 +30,26 @@
     </div>
 
     <script>
+
+        // Функция ymaps.ready() будет вызвана, когда
+                    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+                    ymaps.ready(init);
+
+                    function init() {
+                        // Создание карты.
+                        myMap = new ymaps.Map("location", {
+                            // Координаты центра карты.
+                            // Порядок по умолчанию: «широта, долгота».
+                            // Чтобы не определять координаты центра карты вручную,
+                            // воспользуйтесь инструментом Определение координат.
+                            center: [response['latitude'], response['longitude']],
+                            // Уровень масштабирования. Допустимые значения:
+                            // от 0 (весь мир) до 19.
+                            zoom: 15
+                        });
+                            // let placeMark = new ymaps.Placemark([response['latitude'], response['longitude']]);
+                            // myMap.geoObjects.add(placeMark);
+                    }
     $(document).ready(function()
     {
         let clientID = $('h1').data('clientid');
