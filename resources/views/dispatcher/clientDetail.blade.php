7eea1d4937215cc3dc442333d6f081752f91edb1
Bronyrus <bronyruschannel@gmail.com>
2019-08-15 12:13:16 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -26,30 +26,10 @@
         <div>
             <button class="btn btn-primary display-location">Показать последнее местоположение</button>
         </div>
-        <div id="location"></div>
+        <div id="location" style="width: 600px; height: 400px"></div>
     </div>
 
     <script>
-
-        // Функция ymaps.ready() будет вызвана, когда
-                    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-                    ymaps.ready(init);
-
-                    function init() {
-                        // Создание карты.
-                        myMap = new ymaps.Map("location", {
-                            // Координаты центра карты.
-                            // Порядок по умолчанию: «широта, долгота».
-                            // Чтобы не определять координаты центра карты вручную,
-                            // воспользуйтесь инструментом Определение координат.
-                            center: [response['latitude'], response['longitude']],
-                            // Уровень масштабирования. Допустимые значения:
-                            // от 0 (весь мир) до 19.
-                            zoom: 15
-                        });
-                            // let placeMark = new ymaps.Placemark([response['latitude'], response['longitude']]);
-                            // myMap.geoObjects.add(placeMark);
-                    }
     $(document).ready(function()
     {
         let clientID = $('h1').data('clientid');
@@ -79,8 +59,8 @@
                             // от 0 (весь мир) до 19.
                             zoom: 15
                         });
-                            // let placeMark = new ymaps.Placemark([response['latitude'], response['longitude']]);
-                            // myMap.geoObjects.add(placeMark);
+                            let placeMark = new ymaps.Placemark([response['latitude'], response['longitude']]);
+                            myMap.geoObjects.add(placeMark);
                     }
 
                 },
