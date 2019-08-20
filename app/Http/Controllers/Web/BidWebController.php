a89e6f051544e1abb25d5b81cb809bbdb9d89cca
Bronyrus <bronyruschannel@gmail.com>
2019-08-20 10:43:09 +0300

    Update BidWebController.php

--- app/Http/Controllers/Web/BidWebController.php
+++ app/Http/Controllers/Web/BidWebController.php
@@ -115,6 +115,8 @@ class BidWebController extends BaseWebController
     {
         $bid = Bid::where('id', '=', $id)->first();
 
+        self::translateStatus($bid);
+
         return view("dispatcher.bidDetail", [
             'bid' => $bid
         ]);
