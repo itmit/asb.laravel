d224d9145cf8a9b772b8c87f82b6db39a2ccade6
Bronyrus <bronyruschannel@gmail.com>
2019-08-20 11:27:46 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -50,7 +50,7 @@
                 success: function (response) {
                     $('#location').html('');
                     $('#updated_at').html('');
-                    $('#updated_at').html(response['updated_at']);
+                    $('#updated_at').html('Последнее обновление: ' + response['updated_at']);
                     
                     // Функция ymaps.ready() будет вызвана, когда
                     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
