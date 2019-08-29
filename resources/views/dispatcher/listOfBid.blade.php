759a1c341a72a4956711b8d6b496c91ded6da2aa
Bronyrus <bronyruschannel@gmail.com>
2019-08-29 11:23:07 +0300

    Update listOfBid.blade.php

--- resources/views/dispatcher/listOfBid.blade.php
+++ resources/views/dispatcher/listOfBid.blade.php
@@ -118,7 +118,7 @@
                         for(var i = 0; i < response.length; i++) {
                             result += '<tr>';
                             result += '<td><a href="bid/' + response[i]['id'] + '">' + response[i]['status'] + '</a></td>';
-                            result += '<td>' + response[i]['client']['name'] + '</td>';
+                            result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
                             result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
                             result += '<td>' + response[i]['type'] + '</td>';
                             result += '<td>' + response[i]['created_at'] + '</td>';
