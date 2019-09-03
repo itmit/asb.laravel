f764b2cda4b4157a7f60a52f0f159dab1d560983
Bronyrus <bronyruschannel@gmail.com>
2019-09-03 17:40:52 +0300

    Update bidDetail.blade.php

--- resources/views/dispatcher/bidDetail.blade.php
+++ resources/views/dispatcher/bidDetail.blade.php
@@ -62,7 +62,7 @@
                 $locations.each(function () {
                     let placeMark = new ymaps.Placemark([$(this).data('longitude'), $(this).data('latitude')]);
 
-                    console.log([$('.guard').data('guardlongitude'));
+                    console.log($('.guard').data('guardlongitude'));
 
                     let placeMarkGuard = new ymaps.Placemark([$('.guard').data('guardlongitude'), $('.guard').data('guardlatitude')], {}, {
                         // preset: "islands#circleDotIcon",
