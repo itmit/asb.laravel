af6c406730acf1bc6425772cd9725e357e4c9810
Bronyrus <bronyruschannel@gmail.com>
2019-09-09 12:22:13 +0300

    Update listOfClients.blade.php

--- resources/views/dispatcher/listOfClients.blade.php
+++ resources/views/dispatcher/listOfClients.blade.php
@@ -50,34 +50,34 @@
 
         $(document).ready(function()
         {
-        $(document).on('change', '#selectClientsByType', function() {
-            let selectClientsByType = $('#selectClientsByType').val();
-            $.ajax({
-                headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-                dataType: "json",
-                data: {selectClientsByType: selectClientsByType},
-                url     : 'clients/selectClientsByType',
-                method    : 'post',
-                success: function (response) {
-                    // let result = '';
-                    // for(var i = 0; i < response.length; i++) {
-                    //     result += '<tr>';
-                    //     result += '<td><a href="bid/' + response[i]['id'] + '">' + response[i]['status'] + '</a></td>';
-                    //     result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
-                    //     result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
-                    //     result += '<td>' + response[i]['type'] + '</td>';
-                    //     result += '<td>' + response[i]['created_at'] + '</td>';
-                    //     result += '<td>' + response[i]['updated_at'] + '</td>';
-                    //     result += '</tr>';
+            $(document).on('change', '#selectClientsByType', function() {
+                let selectClientsByType = $('#selectClientsByType').val();
+                $.ajax({
+                    headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                    dataType: "json",
+                    data: {selectClientsByType: selectClientsByType},
+                    url     : 'clients/selectClientsByType',
+                    method    : 'post',
+                    success: function (response) {
+                        // let result = '';
+                        // for(var i = 0; i < response.length; i++) {
+                        //     result += '<tr>';
+                        //     result += '<td><a href="bid/' + response[i]['id'] + '">' + response[i]['status'] + '</a></td>';
+                        //     result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
+                        //     result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
+                        //     result += '<td>' + response[i]['type'] + '</td>';
+                        //     result += '<td>' + response[i]['created_at'] + '</td>';
+                        //     result += '<td>' + response[i]['updated_at'] + '</td>';
+                        //     result += '</tr>'; 
+                        // }
+                        // $('tbody').html(result);
                         console.log(response);
-                    }
-                    // $('tbody').html(result);
-                },
-                error: function (xhr, err) { 
-                    console.log(err + " " + xhr);
-                }
-        });
-        });
+                    },
+                    error: function (xhr, err) { 
+                        console.log(err + " " + xhr);
+                    };
+                });
+            });
         });
     </script>
 @endsection
