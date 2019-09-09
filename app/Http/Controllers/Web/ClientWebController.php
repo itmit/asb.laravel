d9d9c252641289efb56b0ea313d19d95c05062f8
Bronyrus <bronyruschannel@gmail.com>
2019-09-09 15:31:49 +0300

    fix

--- app/Http/Controllers/Web/ClientWebController.php
+++ app/Http/Controllers/Web/ClientWebController.php
@@ -260,61 +260,7 @@ class ClientWebController extends Controller
         }
         if($request->clientType == 'Individual')
         {
-            return
-            ?>
-            <div class="form-group">
-                    <label for="name" class="col-md-4 control-label">ФИО</label>
-    
-                    <div class="col-md-6">
-                        <input id="name" type="text" class="form-control" name="name" value="" required
-                               autofocus>
-                    </div>
-                </div>
-    
-                <div class="form-group">
-                    <label for="email" class="col-md-4 control-label">E-Mail адрес</label>
-    
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" name="email" value=""
-                               required>
-                    </div>
-                </div>
-    
-                <div class="form-group">
-                    <label for="phone_number" class="col-md-4 control-label">Номер телефона</label>
-    
-                    <div class="col-md-6">
-                        <input id="phone_number" type="tel" class="form-control" name="phone_number"
-                               value="" required>
-                    </div>
-                </div>
-    
-                <div class="form-group">
-                    <label for="passport" class="col-md-4 control-label">Серия и номер паспорта</label>
-    
-                    <div class="col-md-6">
-                        <input id="passport" type="text" class="form-control" name="passport"
-                               value="" required>
-                    </div>
-                </div>
-    
-                <div class="form-group">
-                    <label for="password" class="col-md-4 control-label">Пароль</label>
-    
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control" name="password" required>
-                    </div>
-                </div>
-    
-                <div class="form-group">
-                    <label for="password-confirm" class="col-md-4 control-label">Повторите пароль</label>
-    
-                    <div class="col-md-6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                               required>
-                    </div>
-                </div>
-                <?;
+            return view("dispatcher.clientTypes.individual");
         }
     }
     
