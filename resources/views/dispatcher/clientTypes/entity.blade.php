823d8c6fadf06bbc42efc14c554dd95d72662318
Bronyrus <bronyruschannel@gmail.com>
2019-09-09 15:36:33 +0300

    fix

---
+++ resources/views/dispatcher/clientTypes/entity.blade.php
@@ -0,0 +1,70 @@
+<div class="form-group">
+    <label for="organization" class="col-md-4 control-label">Наименование организации</label>
+
+    <div class="col-md-6">
+        <input id="organization" type="text" class="form-control" name="organization" value="" required
+               autofocus>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="email" class="col-md-4 control-label">ИНН</label>
+
+    <div class="col-md-6">
+        <input id="email" type="email" class="form-control" name="email" value=""
+               required>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="email" class="col-md-4 control-label">ОГРН</label>
+
+    <div class="col-md-6">
+        <input id="email" type="email" class="form-control" name="email" value=""
+               required>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="email" class="col-md-4 control-label">E-Mail адрес</label>
+
+    <div class="col-md-6">
+        <input id="email" type="email" class="form-control" name="email" value=""
+               required>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="phone_number" class="col-md-4 control-label">Номер телефона</label>
+
+    <div class="col-md-6">
+        <input id="phone_number" type="tel" class="form-control" name="phone_number"
+               value="" required>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="gendir" class="col-md-4 control-label">Генеральный директор</label>
+
+    <div class="col-md-6">
+        <input id="gendir" type="text" class="form-control" name="gendir"
+               value="" required>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="password" class="col-md-4 control-label">Пароль</label>
+
+    <div class="col-md-6">
+        <input id="password" type="password" class="form-control" name="password" required>
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="password-confirm" class="col-md-4 control-label">Повторите пароль</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+               required>
+    </div>
+</div>
