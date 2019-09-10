f161866536e10d4c5b684cb44157afbb25af7360
Bronyrus <bronyruschannel@gmail.com>
2019-09-10 11:34:19 +0300

    Update listOfClients.blade.php

--- resources/views/dispatcher/listOfClients.blade.php
+++ resources/views/dispatcher/listOfClients.blade.php
@@ -57,30 +57,61 @@
                     url     : 'clients/selectClientsByType',
                     method    : 'post',
                     success: function (response) {
-                        let result = '';
-                        result += '<tr>';
-                        result += '<th><input type="checkbox" name="destroy-all-places" class="js-destroy-all"/></th>';
-                        result += '<th>Название</th>';
-                        result += '<th>Почта</th>';
-                        result += '<th>Номер телефона</th>';
-                        result += '<th>Статус активности</th>';
-                        result += '<th>Дата создания</th>';
-                        result += '</tr>';
-                        $('thead').html(result);
-                        result = '';
-                        for(var i = 0; i < response.length; i++) {
-                            console.log(response[i]);
+                        if(selectClientsByType == 'Entity')
+                        {
+                            let result = '';
                             result += '<tr>';
-                            result += '<td><a href="client/' + response[i]['id'] + '">' + response[i]['organization'] + '</a></td>';
-                        //     result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
-                        //     result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
-                        //     result += '<td>' + response[i]['type'] + '</td>';
-                        //     result += '<td>' + response[i]['created_at'] + '</td>';
-                        //     result += '<td>' + response[i]['updated_at'] + '</td>';
-                            result += '</tr>'; 
+                            result += '<th><input type="checkbox" name="destroy-all-clients" class="js-destroy-all"/></th>';
+                            result += '<th>Название</th>';
+                            result += '<th>Почта</th>';
+                            result += '<th>Номер телефона</th>';
+                            result += '<th>Статус активности</th>';
+                            result += '<th>Дата создания</th>';
+                            result += '</tr>';
+                            $('thead').html(result);
+                            result = '';
+                            for(var i = 0; i < response.length; i++) {
+                                console.log(response[i]);
+                                result += '<tr>';
+                                result += '<td><input type="checkbox" name="destroy-all-clients" class="js-destroy-all"/></td>';
+                            //     result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
+                            //     result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
+                            //     result += '<td>' + response[i]['type'] + '</td>';
+                            //     result += '<td>' + response[i]['created_at'] + '</td>';
+                            //     result += '<td>' + response[i]['updated_at'] + '</td>';
+                                result += '</tr>'; 
+                            }
+                            $('tbody').html(result);
+                            // console.log(response);
                         }
-                        $('tbody').html(result);
-                        // console.log(response);
+                        if(selectClientsByType == 'Individual')
+                        {
+                            let result = '';
+                            result += '<tr>';
+                            result += '<th><input type="checkbox" name="destroy-all-places" class="js-destroy-all"/></th>';
+                            result += '<th>Имя</th>';
+                            result += '<th>Почта</th>';
+                            result += '<th>Номер телефона</th>';
+                            result += '<th>Статус активности</th>';
+                            result += '<th>Дата создания</th>';
+                            result += '</tr>';
+                            $('thead').html(result);
+                            result = '';
+                            for(var i = 0; i < response.length; i++) {
+                                console.log(response[i]);
+                                result += '<tr>';
+                                result += '<td><input type="checkbox" name="destroy-all-clients" class="js-destroy-all"/></td>';
+                            //     result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
+                            //     result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
+                            //     result += '<td>' + response[i]['type'] + '</td>';
+                            //     result += '<td>' + response[i]['created_at'] + '</td>';
+                            //     result += '<td>' + response[i]['updated_at'] + '</td>';
+                                result += '</tr>'; 
+                            }
+                            $('tbody').html(result);
+                            // console.log(response);
+                        }
+                        
                     },
                     error: function (xhr, err) { 
                         console.log(err + " " + xhr);
