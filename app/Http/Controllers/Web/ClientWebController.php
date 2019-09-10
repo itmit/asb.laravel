da9f7b1726f8eeba2e635bc01283192a205ddf1f
Bronyrus <bronyruschannel@gmail.com>
2019-09-10 17:17:53 +0300

    Update ClientWebController.php

--- app/Http/Controllers/Web/ClientWebController.php
+++ app/Http/Controllers/Web/ClientWebController.php
@@ -248,6 +248,7 @@ class ClientWebController extends Controller
     {
         $clients = Client::all()->where('type', '=', $request->selectClientsByType)->sortByDesc('created_at');
         self::translateType($clients);
+        $clients = collect($clients);
         return response()->json($clients); 
     }
 
