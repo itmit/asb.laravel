253794f82447c7f162d9e70807c5bdfee256324f
Bronyrus <bronyruschannel@gmail.com>
2019-09-10 11:38:11 +0300

    ашч

--- app/Http/Controllers/Web/ClientWebController.php
+++ app/Http/Controllers/Web/ClientWebController.php
@@ -235,6 +235,7 @@ class ClientWebController extends Controller
 
     public function selectClientsByType(Request $request)
     {
+        return response()->json($request->selectClientsByType); 
         $clients = Client::all()->where('type', '=', $request->selectClientsByType)->sortByDesc('created_at');
         self::translateType($clients);
         return response()->json($clients); 
