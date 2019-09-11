eaa4b4e3bd9bff1cd01d82ca92795256cc35cf72
Bronyrus <bronyruschannel@gmail.com>
2019-09-11 11:08:13 +0300

    Update clientDetail.blade.php

--- resources/views/dispatcher/clientDetail.blade.php
+++ resources/views/dispatcher/clientDetail.blade.php
@@ -56,7 +56,7 @@
         </div>
         <div class="col-sm-12">
             <div>
-                <img src="{{URL::asset('/storage/nophoto.png')}}" alt="profile Pic" height="200" width="200">
+                <img src="{{URL::asset()}}/storage/nophoto.png" alt="profile Pic" height="200" width="200">
             </div>
             <div>
                 Генеральный директор: {{ $client->director }}
