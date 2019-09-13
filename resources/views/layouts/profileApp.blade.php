64f5f52bb40edfa0496a5f1168295b0405ad351c
Bronyrus <bronyruschannel@gmail.com>
2019-09-13 15:20:37 +0300

    Update profileApp.blade.php

--- resources/views/layouts/profileApp.blade.php
+++ resources/views/layouts/profileApp.blade.php
@@ -115,9 +115,6 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Имеются необработанные тревоги!</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
         </div>
         <div class="modal-body">
           <p>Modal body text goes here.</p>
