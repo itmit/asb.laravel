913fe0b793741637d042d6ac281b517566e64f3e
Bronyrus <bronyruschannel@gmail.com>
2019-09-13 17:38:45 +0300

    Update profileApp.blade.php

--- resources/views/layouts/profileApp.blade.php
+++ resources/views/layouts/profileApp.blade.php
@@ -246,7 +246,7 @@
                     success: function (response) {
                         if(response.length != 0)
                         {
-                            console.log(response);
+                            // console.log(response);
                             if(pathname != '/bid')
                             {
                                 if(openModal == 0)
