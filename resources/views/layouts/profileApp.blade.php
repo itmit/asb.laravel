0ee92b0184013797275b1425530e148a7ec07252
Bronyrus <bronyruschannel@gmail.com>
2019-09-13 17:06:08 +0300

    Update profileApp.blade.php

--- resources/views/layouts/profileApp.blade.php
+++ resources/views/layouts/profileApp.blade.php
@@ -278,7 +278,7 @@
                                     else
                                     {
                                         $.each(response, function(k, v) {
-                                            $('.modal-text').append('!1 Количество активных тревог: ' + response.length)
+                                            $('.modal-text').append('!1 Количество активных тревог: ' + response.id)
                                         });
                                     }
                                 }
