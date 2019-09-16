6fdddeabc324d34722d997474250eb193c8c32d2
Bronyrus <bronyruschannel@gmail.com>
2019-09-16 15:08:40 +0300

    Update BidWebController.php

--- app/Http/Controllers/Web/BidWebController.php
+++ app/Http/Controllers/Web/BidWebController.php
@@ -247,8 +247,6 @@ class BidWebController extends BaseWebController
     {
         $user = Auth::user();
 
-        return $user;
-
         if ($user->hasRole('dispatcher'))
         {
             $bids = Bid::all()->where('status', '=', 'PendingAcceptance')->sortByDesc('created_at');
