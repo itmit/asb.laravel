0df57501709e255e51f9513c33094bf74d6613ba
Bronyrus <bronyruschannel@gmail.com>
2019-09-16 14:24:01 +0300

    Update profileApp.blade.php

--- resources/views/layouts/profileApp.blade.php
+++ resources/views/layouts/profileApp.blade.php
@@ -271,8 +271,14 @@
                                     {
                                         $.each(response, function(k, v) {
                                             $('.modal-text').html('<div><a href="../bid/' + v.id + '">Новая активная тревога!</a> Клиент: <a href="../client/' + v.client.id + '">' + v.client.name + '</a> Дата создания: ' + v.created_at + '</div>');
+                                            $('.modal-text').append('<div id="map" style="width: 600px; height: 400px">map</div>');
+                                            myMap = new ymaps.Map("map", {
+                                            center: [v.location.longitude, v.location.latitude],
+                                            zoom: 15
+                                            });
                                         });
-                                        $('.modal-text').append('<div id="map" style="width: 600px; height: 400px">map</div>');
+                                        
+                                        
                                     }
                                     else
                                     {
