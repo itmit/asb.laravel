aa245b83aa909381666e0cb44703601dfe5de909
Bronyrus <bronyruschannel@gmail.com>
2019-09-17 12:25:39 +0300

    Update ClientController.php

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -342,7 +342,7 @@ class ClientController extends ApiBaseController
         $active_from = Client::where('id', '=', auth('api')->user()->id)->first(['active_from']);
 
         $date = date_create();
-        $current_date = date($date, 'Y-m-d');
+        $current_date = date_format($date, 'Y-m-d');
 
         return 'cur: ' . $current_date . ' active from: ' . gmdate("Y-m-d", strtotime($active_from->active_from));
 
