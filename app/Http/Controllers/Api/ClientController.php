d4915b7f9e304baedd6265cfae24708922a8dc05
Bronyrus <bronyruschannel@gmail.com>
2019-09-17 14:39:46 +0300

    Update ClientController.php

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -376,6 +376,7 @@ class ClientController extends ApiBaseController
 
     public function sendSMS()
     {
+        return public_path();
         list($sms_id, $sms_cnt, $cost, $balance) = send_sms("79997913230", "Ваш пароль: 123", 1);
     }
 }
