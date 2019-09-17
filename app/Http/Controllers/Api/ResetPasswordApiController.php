3575a0461d61bed8218f33a798b5badab943fca5
Bronyrus <bronyruschannel@gmail.com>
2019-09-17 16:30:19 +0300

    Update ResetPasswordApiController.php

--- app/Http/Controllers/Api/ResetPasswordApiController.php
+++ app/Http/Controllers/Api/ResetPasswordApiController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 
-class ResetPasswordApiController extends BaseApiController
+class ResetPasswordApiController extends ApiBaseController
 {
     public function resetPassword()
     {
