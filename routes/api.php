3f747f755fcbda03de28ba93e1ab57f2760f47c2
Bronyrus <bronyruschannel@gmail.com>
2019-09-17 15:49:56 +0300

    sms

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'Api\ClientController@login');
 Route::post('register', 'Api\ClientController@register');
+Route::post('resetPassword', 'Api\ClientController@resetPassword');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('details', 'Api\ClientController@details');
