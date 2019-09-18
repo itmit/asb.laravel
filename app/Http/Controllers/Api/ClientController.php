40cc479d247cf7f04cd253009d5e1ef69a53183c
Bronyrus <bronyruschannel@gmail.com>
2019-09-18 12:55:56 +0300

    Update ClientController.php

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -395,8 +395,7 @@ class ClientController extends ApiBaseController
 
             if($current_date == gmdate("Y-m-d", strtotime("+27 day", $active_client->active_from)))
             {
-                // return 'time to sms';
-                echo gmdate("Y-m-d", strtotime("+27 day", $active_client->active_from));
+                return gmdate("Y-m-d", strtotime("+27 day", $active_client->active_from));
             }
 
             if($current_date_unix > strtotime("+30 day", $active_client->active_from))
@@ -405,6 +404,5 @@ class ClientController extends ApiBaseController
             }
         }
 
-        return $current_date;
     }
 }
