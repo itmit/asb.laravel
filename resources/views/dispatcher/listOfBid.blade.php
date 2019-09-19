521ad96c3eab5a4734d96b606384dfc65951230c
Bronyrus <bronyruschannel@gmail.com>
2019-09-19 18:43:50 +0300

    Update listOfBid.blade.php

--- resources/views/dispatcher/listOfBid.blade.php
+++ resources/views/dispatcher/listOfBid.blade.php
@@ -14,8 +14,8 @@
             </select>
 
             <ul class="nav nav-tabs" id="myTab">
-                <li  data-type="PendingAcceptance" class="active"><a href="#">Ожидает принятия</a></li>
-                <li data-type="Accepted><a href="#"">Принята</a></li>
+                <li data-type="PendingAcceptance" class="active"><a href="#">Ожидает принятия</a></li>
+                <li data-type="Accepted"><a href="#">Принята</a></li>
                 <li data-type="Processed"><a href="#">Выполнена</a></li>
             </ul>
 
@@ -148,6 +148,31 @@
             e.preventDefault()
             $(this).tab('show')
             console.log($(this).data('type'))
+            let selectBidsByStatus = $(this).data('type');
+            $.ajax({
+                headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                dataType: "json",
+                data: {selectBidsByStatus: selectBidsByStatus},
+                url     : 'bid/updateList',
+                method    : 'post',
+                success: function (response) {
+                    let result = '';
+                        for(var i = 0; i < response.length; i++) {
+                            result += '<tr>';
+                            result += '<td><a href="bid/' + response[i]['id'] + '">' + response[i]['status'] + '</a></td>';
+                            result += '<td><a href="client/' + response[i]['client']['id'] + '">' + response[i]['client']['name'] + '</a></td>';
+                            result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
+                            result += '<td>' + response[i]['type'] + '</td>';
+                            result += '<td>' + response[i]['created_at'] + '</td>';
+                            result += '<td>' + response[i]['updated_at'] + '</td>';
+                            result += '</tr>';
+                        }
+                        $('tbody').html(result);
+                },
+                error: function (xhr, err) { 
+                    console.log(err + " " + xhr);
+                }
+            });
         })
     </script>
     <script>
