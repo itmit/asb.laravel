6c00ba9afc3df185de5ac73cb60c218910f71fbd
Bronyrus <bronyruschannel@gmail.com>
2019-09-19 16:53:53 +0300

    Update RepresentativeWebController.php

--- app/Http/Controllers/Web/RepresentativeWebController.php
+++ app/Http/Controllers/Web/RepresentativeWebController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
+use Illuminate\Validation\Rule;
 
 class RepresentativeWebController extends Controller
 {
