57c8c7d5fab521ac1d85829b1ecc12f251fadb08
Bronyrus <bronyruschannel@gmail.com>
2019-09-20 16:10:53 +0300

    Update guardDetail.blade.php

--- resources/views/representative/guardDetail.blade.php
+++ resources/views/representative/guardDetail.blade.php
@@ -2,4 +2,15 @@
 
 @section('content')
     <h1 id="guard" data-clientid="{{ $guard->id }}">ГБР</h1>
+    <div class="col-sm-12">
+        <div>
+            Наименование: {{ $guard->name }}
+        </div>
+        <div>
+            Почта: {{ $guard->email }}
+        </div>
+        <div>
+            Телефон: {{ $guard->phone_number }}
+        </div>
+    </div>
 @endsection
