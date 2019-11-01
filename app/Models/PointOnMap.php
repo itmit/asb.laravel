a98956b00a360ec1f66123ef8a4752c2d7315a5c
Bronyrus <bronyruschannel@gmail.com>
2019-11-01 15:29:41 +0300

    f

--- app/Models/PointOnMap.php
+++ app/Models/PointOnMap.php
@@ -7,21 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class PointOnMap extends Model
 {
     /**
-     * @var array
-     */
-    protected $fillable = [
-        'client',
-        'latitude',
-        'longitude',
-    ];
-
-    /**
      * @var string
      */
     protected $table = 'point_on_map';
 
-    public function client() 
-    {
-        return $this->belongsTo(Client::class, 'client')->get()->first();        
-    }
+    protected $guarded = ['id'];
+
+    // public function client() 
+    // {
+    //     return $this->belongsTo(Client::class, 'client')->get()->first();        
+    // }
 }
