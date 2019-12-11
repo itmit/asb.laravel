5a107461b91e56527e1ae6894f4e46dd9fae3f39
Bronyrus <bronyruschannel@gmail.com>
2019-12-11 17:31:33 +0300

    Update BidApiController.php

--- app/Http/Controllers/Api/BidApiController.php
+++ app/Http/Controllers/Api/BidApiController.php
@@ -22,125 +22,125 @@ class BidApiController extends ApiBaseController
      */
     public function index(Request $request) : JsonResponse
     {
-        if(request('status'))
-        {
-            // $bids = DB::table('bid')
-            // ->leftJoin('point_on_map', 'bid.client', '=', 'point_on_map.client')
-            // ->join('clients', 'bid.client', '=', 'clients.id')
-            // ->join('users', 'clients.representative', '=', 'users.id')
-            // // ->where('clients.representative', '=', $this->getRepresentativeId())
-            // ->where('bid.status', '=', request('status'))
-            // ->select('bid.status', 'point_on_map.latitude', 'point_on_map.longitude', 'clients.name', 'clients.email',
-            //     'clients.phone_number', 'clients.organization', 'bid.created_at', 'bid.updated_at', 'bid.uid', 'clients.note', 'clients.user_picture', 'bid.type')
-            // ->orderBy('bid.created_at', 'desc')
-            // ->distinct()
-            // ->get();
-            $bids = Bid::all()->where('status', '=', 'PendingAcceptance')->sortByDesc('created_at');
-        }
-        else 
-        {
-            // $bids = DB::table('bid')
-            // ->leftJoin('point_on_map', 'bid.client', '=', 'point_on_map.client')
-            // ->leftJoin('clients', 'bid.client', '=', 'clients.id')
-            // ->join('users', 'clients.representative', '=', 'users.id')
-            // // ->where('clients.representative', '=', $this->getRepresentativeId())
-            // ->whereIn('client', function($query){
-            //     $query->select('paper_type_id')
-            //     ->from(with(new ProductCategory)->getTable())
-            //     ->whereIn('category_id', ['223', '15'])
-            //     ->where('active', 1);
-            // })
-            // ->select('bid.status', 'point_on_map.latitude', 'point_on_map.longitude', 'clients.name', 'clients.email',
-            //     'clients.phone_number', 'clients.organization', 'bid.updated_at', 'clients.note', 'clients.user_picture', 'bid.created_at', 'bid.uid', 'bid.type')
-            // ->orderBy('bid.created_at', 'desc')
-            // ->distinct()
-            // ->get();
-            $bids = Bid::all()->sortByDesc('created_at');
-        }
+        // if(request('status'))
+        // {
+        //     // $bids = DB::table('bid')
+        //     // ->leftJoin('point_on_map', 'bid.client', '=', 'point_on_map.client')
+        //     // ->join('clients', 'bid.client', '=', 'clients.id')
+        //     // ->join('users', 'clients.representative', '=', 'users.id')
+        //     // // ->where('clients.representative', '=', $this->getRepresentativeId())
+        //     // ->where('bid.status', '=', request('status'))
+        //     // ->select('bid.status', 'point_on_map.latitude', 'point_on_map.longitude', 'clients.name', 'clients.email',
+        //     //     'clients.phone_number', 'clients.organization', 'bid.created_at', 'bid.updated_at', 'bid.uid', 'clients.note', 'clients.user_picture', 'bid.type')
+        //     // ->orderBy('bid.created_at', 'desc')
+        //     // ->distinct()
+        //     // ->get();
+        //     $bids = Bid::all()->where('status', '=', 'PendingAcceptance')->sortByDesc('created_at');
+        // }
+        // else 
+        // {
+        //     // $bids = DB::table('bid')
+        //     // ->leftJoin('point_on_map', 'bid.client', '=', 'point_on_map.client')
+        //     // ->leftJoin('clients', 'bid.client', '=', 'clients.id')
+        //     // ->join('users', 'clients.representative', '=', 'users.id')
+        //     // // ->where('clients.representative', '=', $this->getRepresentativeId())
+        //     // ->whereIn('client', function($query){
+        //     //     $query->select('paper_type_id')
+        //     //     ->from(with(new ProductCategory)->getTable())
+        //     //     ->whereIn('category_id', ['223', '15'])
+        //     //     ->where('active', 1);
+        //     // })
+        //     // ->select('bid.status', 'point_on_map.latitude', 'point_on_map.longitude', 'clients.name', 'clients.email',
+        //     //     'clients.phone_number', 'clients.organization', 'bid.updated_at', 'clients.note', 'clients.user_picture', 'bid.created_at', 'bid.uid', 'bid.type')
+        //     // ->orderBy('bid.created_at', 'desc')
+        //     // ->distinct()
+        //     // ->get();
+        //     $bids = Bid::all()->sortByDesc('created_at');
+        // }
 
-        $response = [];
-        $clients = [];
-        $i=0;
-
-        foreach ($bids as $bid) {
-            if($bid->client()->location() == NULL) continue;
-            $clients[$i] = $bid->client();
-            $response[] = [
-                'uid'   => $bid->uid,
-                'status' => $bid->status,
-                'type' => $bid->type,
-                'updated_at' => date('Y-m-d H:i:s', strtotime($bid->updated_at)),
-                'created_at' => date('Y-m-d H:i:s', strtotime($bid->created_at)),
-                'location' => [
-                    'latitude' => $bid->latitude,
-                    'longitude' => $bid->longitude
-                ],
-                'client' => [
-                    'type' => $clients[$i]['type'],
-                    'name' => $clients[$i]['name'],
-                    'email' => $clients[$i]['email'],
-                    'phone_number' => $clients[$i]['phone_number'],
-                    'organization' => $clients[$i]['organization'],
-                    'note' => $clients[$i]['note'], 
-                    'user_picture' => $clients[$i]['user_picture'],
-                    'passport' => $clients[$i]['passport'],
-                    'INN' => $clients[$i]['INN'],
-                    'OGRN' => $clients[$i]['OGRN'],
-                    'director' => $clients[$i]['director']
-                ]
-            ];
-            $i++;
-        }
-
-        return $this->sendResponse(
-            $response,
-            'Bids retrieved successfully.'
-        );
-
-        // $bids = Bid::select('id', 'uid', 'client','status', 'type', 'updated_at', 'created_at', 'latitude', 'longitude')
-        //     ->when(request('status'), function ($query) {
-        //     return $query->where('status', 'PendingAcceptance');
-        // })
-        // ->orderBy('created_at', 'desc')
-        // ->get();
-        
         // $response = [];
-        // $currentClient = null;
-        
+        // $clients = [];
+        // $i=0;
+
         // foreach ($bids as $bid) {
-        //     $currentClient = $bid->client();
-        //     if($currentClient->location() == NULL) continue;
-            
+        //     if($bid->client()->location() == NULL) continue;
+        //     $clients[$i] = $bid->client();
         //     $response[] = [
-        //         'uid' => $bid->uid,
+        //         'uid'   => $bid->uid,
         //         'status' => $bid->status,
         //         'type' => $bid->type,
         //         'updated_at' => date('Y-m-d H:i:s', strtotime($bid->updated_at)),
         //         'created_at' => date('Y-m-d H:i:s', strtotime($bid->created_at)),
         //         'location' => [
-        //         'latitude' => $bid->latitude,
-        //         'longitude' => $bid->longitude
-        //     ],
+        //             'latitude' => $bid->latitude,
+        //             'longitude' => $bid->longitude
+        //         ],
         //         'client' => [
-        //         'type' => $currentClient['type'],
-        //         'name' => $currentClient['name'],
-        //         'email' => $currentClient['email'],
-        //         'phone_number' => $currentClient['phone_number'],
-        //         'organization' => $currentClient['organization'],
-        //         'note' => $currentClient['note'],
-        //         'user_picture' => $currentClient['user_picture'],
-        //         'passport' => $currentClient['passport'],
-        //         'INN' => $currentClient['INN'],
-        //         'OGRN' => $currentClient['OGRN'],
-        //         'director' => $currentClient['director']
-        //     ]
+        //             'type' => $clients[$i]['type'],
+        //             'name' => $clients[$i]['name'],
+        //             'email' => $clients[$i]['email'],
+        //             'phone_number' => $clients[$i]['phone_number'],
+        //             'organization' => $clients[$i]['organization'],
+        //             'note' => $clients[$i]['note'], 
+        //             'user_picture' => $clients[$i]['user_picture'],
+        //             'passport' => $clients[$i]['passport'],
+        //             'INN' => $clients[$i]['INN'],
+        //             'OGRN' => $clients[$i]['OGRN'],
+        //             'director' => $clients[$i]['director']
+        //         ]
         //     ];
+        //     $i++;
         // }
-        
+
         // return $this->sendResponse(
         //     $response,
         //     'Bids retrieved successfully.'
-        //     );
+        // );
+
+        $bids = Bid::select('id', 'uid', 'client','status', 'type', 'updated_at', 'created_at', 'latitude', 'longitude')
+            ->when(request('status'), function ($query) {
+            return $query->where('status', 'PendingAcceptance');
+        })
+        ->orderBy('created_at', 'desc')
+        ->get();
+        
+        $response = [];
+        $currentClient = null;
+        
+        foreach ($bids as $bid) {
+            $currentClient = $bid->client();
+            if($currentClient->location() == NULL) continue;
+            
+            $response[] = [
+                'uid' => $bid->uid,
+                'status' => $bid->status,
+                'type' => $bid->type,
+                'updated_at' => date('Y-m-d H:i:s', strtotime($bid->updated_at)),
+                'created_at' => date('Y-m-d H:i:s', strtotime($bid->created_at)),
+                'location' => [
+                'latitude' => $bid->latitude,
+                'longitude' => $bid->longitude
+            ],
+                'client' => [
+                'type' => $currentClient['type'],
+                'name' => $currentClient['name'],
+                'email' => $currentClient['email'],
+                'phone_number' => $currentClient['phone_number'],
+                'organization' => $currentClient['organization'],
+                'note' => $currentClient['note'],
+                'user_picture' => $currentClient['user_picture'],
+                'passport' => $currentClient['passport'],
+                'INN' => $currentClient['INN'],
+                'OGRN' => $currentClient['OGRN'],
+                'director' => $currentClient['director']
+            ]
+            ];
+        }
+        
+        return $this->sendResponse(
+            $response,
+            'Bids retrieved successfully.'
+            );
         
 
     }
