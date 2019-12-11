fbeef15c20eaec1a1390b1fc85a404ec8e7f5f76
Bronyrus <bronyruschannel@gmail.com>
2019-12-11 15:09:52 +0300

    Update BidApiController.php

--- app/Http/Controllers/Api/BidApiController.php
+++ app/Http/Controllers/Api/BidApiController.php
@@ -94,7 +94,7 @@ class BidApiController extends ApiBaseController
 
 
         return $this->sendResponse(
-            $clients[$i]['id'],
+            $clients[$i],
             'Bids retrieved successfully.'
         );
 
